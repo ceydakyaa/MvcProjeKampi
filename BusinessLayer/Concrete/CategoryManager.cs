025516e3d5fef70ffc35e17c7ccd8dc21083ca4f
ceydakyaa <88967224+ceydakyaa@users.noreply.github.com>
2025-04-06 23:37:54 +0300

    devam edildi.

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -11,13 +11,13 @@ namespace BusinessLayer.Concrete
     public class CategoryManager
     {
         GenericRepository<Category> repo = new GenericRepository<Category>();
-        public List<Category> GetAll()
+        public List<Category> GetAllBL()
         {
             return repo.List();
         }
         public void CategoryAddBL(Category p)
         {
-            if(p.CategoryName== "" || p.CategoryName.Length <=3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
+            if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
             {
                 //hata mesajı
             }
